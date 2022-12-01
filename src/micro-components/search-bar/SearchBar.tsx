cd71df6dc776ff303b73f0a9b553d3ec6ff972c2
Stephen Riding <ridingjr@gmail.com>
2022-11-30 19:17:59 -0800

    working on search bar...

--- src/micro-components/search-bar/SearchBar.tsx
+++ src/micro-components/search-bar/SearchBar.tsx
@@ -1,7 +1,16 @@
+import searchIcon from "../../resources/go.png";
+
 const SearchBar = () => {
   return (
     <form className="SearchBar">
       <input type="text" placeholder="Hello world." />
+      <input
+        type="image"
+        src={searchIcon}
+        alt="Search Icon"
+        className="GLOBAL-SEARCH-IMAGE"
+        onClick={() => console.log("hello!")}
+      />
     </form>
   );
 };
--- src/services/GetPostsService.ts
+++ src/services/GetPostsService.ts
@@ -80,7 +80,7 @@ class GetPostRequest {
   public async requestMultiplePostsWithStartingIndex(
     url: string,
     postId: number
-  ) {
+  ): Promise<PostsWithCompaniesAndJobs[]> {
     try {
       // Fetch posts information from api
       const postInformationReponse = await fetch(url + postId, {
@@ -133,6 +133,63 @@ class GetPostRequest {
       }
     } catch (error) {
       console.log(error);
+      throw error;
+    }
+  }
+
+  public async requestFilteredPosts(
+    url: string,
+    postId: number,
+    text: string
+  ): Promise<PostsWithCompaniesAndJobs[]> {
+    try {
+      const request = await fetch(url + postId + "/filtered/by/" + text, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      });
+
+      const response: APIResponsePayloadType = await request.json();
+
+      if (response._success) {
+        // Parameter fields must match response payload from api
+        const postCompanyJobArray: PostsWithCompaniesAndJobs[] = [];
+
+        if (response._payload.length === 0) {
+          return [];
+        }
+
+        response._payload.forEach((entry: typeof response._payload) => {
+          const newPost: Post = new Post(entry._post_notes, entry._post_id);
+          postCompanyJobArray.push({
+            post: newPost,
+            company: new Company(
+              entry._company_name,
+              entry._company_website,
+              entry._company_information,
+              entry._company_id,
+              newPost
+            ),
+            job: new Job(
+              entry._job_title,
+              entry._job_information,
+              entry._job_location,
+              entry._job_type,
+              entry._job_status,
+              entry._job_application_submitted_date,
+              entry._job_application_dismissed_date,
+              entry._job_id,
+              newPost
+            ),
+          });
+        });
+
+        return postCompanyJobArray;
+      } else {
+        throw new GetPostsFailedException(response._message);
+      }
+    } catch (error) {
       throw error;
     }
   }
